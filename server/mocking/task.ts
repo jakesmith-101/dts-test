446d410461cabb921c1e3c170c441e15e681c146
jakesmith-101 <jakesmith@binaryrage.com>
2025-04-14 16:03:38 +0100

    fix db data type

--- server/mocking/task.ts
+++ server/mocking/task.ts
@@ -1,5 +1,5 @@
 import pool from "../db";
 
 export function CreateTaskTable() {
-    pool.query("CREATE TABLE IF NOT EXISTS tasks (id int(11) NOT NULL auto_increment, title varchar(100) NOT NULL default '', description varchar(250), status boolean NOT NULL default false , due timestamp NOT NULL default now(), PRIMARY KEY (id));");
+    pool.query("CREATE TABLE IF NOT EXISTS tasks (id integer NOT NULL auto_increment, title varchar(100) NOT NULL default '', description varchar(250), status boolean NOT NULL default false , due timestamp NOT NULL default now(), PRIMARY KEY (id));");
 }
